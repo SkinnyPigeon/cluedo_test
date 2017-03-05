a022736ebf086237cd42e178dc53607e2e1543bd
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2017-03-05 13:42:39 +0000

    player starts without cards

--- client/src/models/player.js
+++ client/src/models/player.js
@@ -1,11 +1,16 @@
 var Player = function( name ) {
   this.name = name;
+  this.cards = [];
 };
 
 Player.prototype = {
 
   playerName: function() {
     return this.name;
+  },
+
+  noOfCards: function() {
+    return this.cards.length;
   }
 };
 
--- client/src/models/specs/player_spec.js
+++ client/src/models/specs/player_spec.js
@@ -5,10 +5,15 @@ describe( "The player: ", function() {
 
   beforeEach( function() {
     p1 = new Player( "Euan" );
+    p2 = new Player( "Euan" );
   });
 
   it( "Should start with a player name", function() {
     assert.equal( "Euan", p1.playerName() );
+  });
+
+  it( "Should start with no cards", function() {
+    assert.equal( 0, p2.noOfCards() );
   });
 
 });
